f24b9ae437ac9ba913862f45e2fd66893e7fdaae
afolabiopakunle <opakunleafolabi@gmail.com>
2020-08-06 19:13:29 +0100

    post route working...

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("home")
+    res.render("home");
 })
 
+app.get("/students", (req, res) => {
+    let students = ["Desire","Ibukun","Adebola"]
+    res.render("students", {students});
+})
+
+app.post("/addStudent", (req, res) => {
+    res.send("Post Route Reached")
+})
 
 
 app.listen(3000, () => console.log('app running on port 3000'))
